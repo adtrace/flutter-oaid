7d1b1ba47c455d7af4f023586fe64ddffa07d707
Nasser Amini <namini40@gmail.com>
2023-12-24 14:04:03 +0330

    feat: add oaid support

--- android/app/src/main/java/com/example/adtrace_flutter_oaid/MainActivity.java
+++ android/app/src/main/java/com/example/adtrace_flutter_oaid/MainActivity.java
@@ -1,6 +1,17 @@
 package com.example.adtrace_flutter_oaid;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import io.adtrace.sdk.oaid.AdTraceOaid;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AdTraceOaid.readOaid(this);
+    }
 }
